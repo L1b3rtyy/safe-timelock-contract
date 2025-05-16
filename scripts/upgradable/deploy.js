0ae3d0b7517ad9c21862e2939809a6a6cdf73e32
olivierkrener <olivier.krener@gmail.com>
2025-05-16 15:07:14 +0200

    Log implementation contract address  to the console

--- scripts/upgradable/deploy.js
+++ scripts/upgradable/deploy.js
@@ -8,8 +8,11 @@ async function main() {
 
   const Guard = await ethers.getContractFactory("TimelockGuardUpgradeable", deployer);
   const proxy = await upgrades.deployProxy(Guard, argumentsArray, { initializer: 'initialize' });
-
   console.log("Guard proxy deployed at:", proxy.address);
+  await proxy.deployed();
+  
+  const impAddress = await upgrades.erc1967.getImplementationAddress(proxy.address);
+  console.log("Guard implementation deployed at:", impAddress);
 
   const admin = await upgrades.admin.getInstance();
   console.log("Guard proxy admin deployed at:", admin.address);
